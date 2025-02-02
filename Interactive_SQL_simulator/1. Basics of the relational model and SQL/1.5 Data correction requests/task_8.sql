7744352a3d982482660b9dfa2d809d7b4af5d963
Sergey Shmakov <sergey-sti36@yandex.ru>
2025-02-02 15:24:49 +0300

    completed section 1.4

---
+++ Interactive_SQL_simulator/1. Basics of the relational model and SQL/1.5 Data correction requests/task_8.sql
@@ -0,0 +1,11 @@
+/*Задание
+Удалить из таблицы supply книги тех авторов, общее количество экземпляров книг которых в таблице book превышает 10.*/
+
+DELETE FROM supply
+WHERE author IN (
+    SELECT author
+    FROM book
+    GROUP BY author
+    HAVING SUM(amount) > 10
+);
+SELECT * FROM supply
