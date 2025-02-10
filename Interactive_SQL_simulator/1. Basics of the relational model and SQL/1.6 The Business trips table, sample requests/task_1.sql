44a1cd2cb84da7a3e178f510f95c1d992d3fbb8c
Sergey Shmakov <sergey-sti36@yandex.ru>
2025-02-10 15:22:59 +0300

    completed section 1.6

---
+++ Interactive_SQL_simulator/1. Basics of the relational model and SQL/1.6 The Business trips table, sample requests/task_1.sql
@@ -0,0 +1,7 @@
+/*Задание
+Вывести из таблицы trip информацию о командировках тех сотрудников, фамилия которых заканчивается на букву «а», в отсортированном по убыванию даты последнего дня командировки виде. В результат включить столбцы name, city, per_diem, date_first, date_last.*/
+
+SELECT name, city, per_diem, date_first, date_last
+FROM trip
+WHERE name LIKE "%а ____"
+ORDER BY date_last DESC
