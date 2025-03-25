e66e49739e57de157dc33a363b676c98afe42524
Sergey Shmakov <sergey.shmakov2@icl-services.com>
2025-03-25 17:34:53 +0300

    completed section 3.2

---
+++ Interactive_SQL_simulator/3. Databases and SQL queries/3.2 Testing database, adjustment requests/task_1.sql
@@ -0,0 +1,8 @@
+/*Задание
+В таблицу attempt включить новую попытку для студента Баранова Павла по дисциплине «Основы баз данных». Установить текущую дату в качестве даты выполнения попытки.*/
+
+INSERT INTO attempt(student_id, subject_id, date_attempt)
+SELECT 
+    (SELECT student_id FROM student WHERE name_student = 'Баранов Павел'), 
+    (SELECT subject_id FROM subject WHERE name_subject = 'Основы баз данных'), 
+    NOW();
