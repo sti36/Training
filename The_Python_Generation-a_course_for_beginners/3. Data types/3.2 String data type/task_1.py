53db7f67324ee813c5db7ec2f21f90c01ed5ea53
Sergey_Shmakov_ICL <Sergey.Shmakov2@icl-services.com>
2025-04-17 12:32:27 +0300

    completed 5 tasks

---
+++ The_Python_Generation-a_course_for_beginners/3. Data types/3.2 String data type/task_1.py
@@ -0,0 +1,10 @@
+'''Напишите программу, которая выводит текст:
+
+"Python is a great language!", said Fred. "I don't ever remember having this much fun before."
+Примечание. Используйте конкатенацию строк.'''
+
+line_one = '"Python is a great language!", said Fred. "I don'
+line_two = "'"
+line_three = 't ever remember having this much fun before."'
+
+print(line_one + line_two + line_three)
