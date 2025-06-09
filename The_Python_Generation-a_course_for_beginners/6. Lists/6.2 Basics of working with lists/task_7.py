6c141f788c982cedc1099eab471ef76ae43cedb9
Sergey_Shmakov_ICL <Sergey.Shmakov2@icl-services.com>
2025-06-09 11:02:55 +0300

    completed section 6.2

---
+++ The_Python_Generation-a_course_for_beginners/6. Lists/6.2 Basics of working with lists/task_7.py
@@ -0,0 +1,6 @@
+"""Дополните приведённый ниже код так, чтобы он вывел "перевёрнутый" список languages (то есть элементы будут идти в обратном порядке).
+
+Примечание. Для вывода списка воспользуйтесь функцией print()."""
+
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+print(languages[::-1])
